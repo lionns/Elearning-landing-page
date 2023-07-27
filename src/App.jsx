5e01727da269ff023674210833646f9ecb567890
Juan León <juans.leonv@icloud.com>
2023-07-26 22:16:03 -0500

    feat: creating hero part with custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import imgHero from "./assets/image-hero-mobile@2x.png";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-    <div className="w-10 h-10 bg-pink-gradient"></div>
-      <h1 className="text-3xl font-bold underline bg-purple">Hello world!</h1>
-    </>
+    <div className="p-4">
+      <header className="flex justify-between items-center">
+        <span className="text-[1.75rem] font-extrabold text-dark-purple">
+          skilled
+        </span>
+        <button className="text-b-sm bg-dark-purple text-white px-6 py-2.5 rounded-[1.75rem]">
+          Get Started
+        </button>
+      </header>
+      <h1 className="text-h-lg text-dark-purple mt-[38px]">
+        Maximize skill, minimize budget
+      </h1>
+      <p className="text-b-sm leading-[26px] text-gray mt-[26px] mb-6">
+        Our modern courses across a range of in-demand skills will give you the
+        knowledge you need to live the life you want.
+      </p>
+      <button className="text-b-sm bg-pink-gradient text-white px-6 py-2.5 rounded-[1.75rem]">
+        Get Started
+      </button>
+      <img className="w-full" src={imgHero} alt="Image hero" />
+    </div>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   theme: {
     extend: {},
     backgroundImage: {
+      "white-gradient": "linear-gradient(180deg, #FFF 0%, #F0F1FF 100%)",
       "pink-gradient": "linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)",
       "purple-gradient": "linear-gradient(180deg, #4851FF 0%, #F02AA6 99.92%)",
     },
@@ -17,6 +18,50 @@ export default {
     },
     fontFamily: {
       sans: "Plus Jakarta Sans",
+    },
+    fontSize: {
+      "b-sm": [
+        "1rem",
+        {
+          fontWeight: 500,
+          lineHeight: "1.75rem",
+        },
+      ],
+      "b-md": [
+        "1.125rem",
+        {
+          fontWeight: 500,
+          lineHeight: "1.75rem",
+        },
+      ],
+      "h-sm": [
+        "1.5rem",
+        {
+          fontWeight: 800,
+          lineHeight: "1.75rem",
+        },
+      ],
+      "h-md": [
+        "2rem",
+        {
+          fontWeight: 800,
+          lineHeight: "2.5rem",
+        },
+      ],
+      "h-lg": [
+        "2.5rem",
+        {
+          fontWeight: 800,
+          lineHeight: "3.1875rem",
+        },
+      ],
+      "h-xl": [
+        "3.5rem",
+        {
+          fontWeight: 800,
+          lineHeight: "4.375rem",
+        },
+      ],
     },
   },
   plugins: [],
